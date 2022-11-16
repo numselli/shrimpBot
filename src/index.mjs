003c9a1ec3b8d7ec1c5dbdce9a7f57bb79599259
numselli <58607232+numselli@users.noreply.github.com>
2022-11-16 09:58:17 -0800

    add files

---
+++ src/index.mjs
@@ -0,0 +1,28 @@
+import { Client } from "oceanic.js"
+import { Token } from "/static/config.mjs"
+
+const client = new Client({
+    auth: Token,
+    gateway: {
+        intents: ["GUILDS", "GUILD_MESSAGES", "MESSAGE_CONTENT"]
+    }
+});
+
+client.on("ready", () => console.log("Ready as", client.user.tag));
+
+// An error handler
+client.on("error", (error) => {
+    console.error("Something went wrong:", error);
+});
+
+const shrimpChars = ["s", "h", "r", "i", "m", "p"]
+// Message Sent
+bot.on("messageCreate", (msg) => {
+    const lowerMsg = msg.content.toLowerCase()
+    if (shrimpChars.some(char=>lowerMsg.includes(char))){
+        msg.createReaction("🦐").catch(err=>{})
+    }
+});
+
+// Connect to Discord
+client.connect();
